27728b14f72c38cfddac6c594f04e22eb20996df
victor mathias <victormathias001@gmail.com>
2022-09-02 09:10:07 +0100

    firs commit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,10 @@ import "./App.css";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { FaRegCopyright } from "react-icons/fa";
 import styled from "styled-components";
-
+import NewsletterSubscribe from "./NewsLetterSubscribe";
 const Center = styled.div`
   margin: 20px auto;
 `;
@@ -25,26 +24,14 @@ function App() {
                 height="30px"
                 className="d-inline-block align-top"
               />{" "}
-              React-Bootstrap
+              Jopasavvy
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="me-auto">
                 <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#link">Link</Nav.Link>
-                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">
-                    Another action
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.3">
-                    Something
-                  </NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action/3.4">
-                    Separated link
-                  </NavDropdown.Item>
-                </NavDropdown>
+                <Nav.Link href="#link">About us</Nav.Link>
+                <Nav.Link href="#link">Contact</Nav.Link>
               </Nav>
             </Navbar.Collapse>
           </Container>
@@ -73,7 +60,7 @@ function App() {
           </p>
         </div>
         <div>
-          <h4>Sign up</h4>
+          <NewsletterSubscribe />
         </div>
       </div>
       <div className="item1">
@@ -106,9 +93,25 @@ function App() {
         </div>
       </div>
       <div className="item2">
-        <h4>Jopasavvy</h4>
-        <h4>Quick Links</h4>
-        <h4>Follow Us</h4>
+        <div>
+          <h4>Jopasavvy</h4>
+          <h9>The new way to buy</h9>
+        </div>
+        <div>
+          <h4>Quick Links</h4>
+          <h6>Home</h6>
+          <h6>About</h6> <h6>Contact</h6>
+        </div>
+        <div>
+          <h4>Follow Us</h4>
+          <h6>
+            <a href="https://web.facebook.com/Jopa_Savvy">Facebook</a>
+          </h6>
+          <h6 href="https://twitter.com/Jopasavvy">Twitter</h6>
+          <a href="https://www.instagram.com/Jopasavvy_GL/">
+            <h6>Instagram</h6>
+          </a>
+        </div>
       </div>
       <div className="footer">
         <footerSection>
